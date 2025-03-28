28dec44f54c4319091cfad3b43e0053ec9bcd854
Siddhant07-Meesho <siddhant.shekhar@meesho.com>
2025-03-28 16:52:49 +0530

    Added feedContext

--- widgetHandler.py
+++ widgetHandler.py
@@ -24,7 +24,7 @@ def get_cross_sell_recommendations(product_id, user_id="6105390", limit=10, scre
                 "product_ids": [product_id]
             },
             "user_id": user_id,
-            "tenant": "CROSS_SELL"
+            "feedContext": "CROSS_SELL"
         }
     else:
         payload = {
@@ -34,7 +34,7 @@ def get_cross_sell_recommendations(product_id, user_id="6105390", limit=10, scre
                 "catalog_ids": [product_id]
             },
             "user_id": user_id,
-            "tenantContext": "CROSS_SELL"
+            "feedContext": "CROSS_SELL"
         }
 
 
